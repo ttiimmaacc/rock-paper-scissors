471dca4427c799c165d46b2e88f82292aadd6fda
Tim Mac <ttiimmaacc@MacBook-Pro.local>
2020-04-04 16:41:01 +1100

    create a function in "expression mode" - aka an anonymous function

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
+// function startGame() {
+//   console.log('Game is starting...');
+// }
+// Standalone function statement
+
+const start = function() {
   console.log('Game is starting...');
-}
-// Standalone function
+};
+// Anonymous function expression
+// The function keyword can be used to define a function inside an expression. Stores a function inside a variable.
 
 // const person = {
+//   name: 'Max',
 //   greet: function greet() {
 //     console.log('Hello there!');
 //   }
@@ -16,6 +23,8 @@ function startGame() {
 // person.greet();
 // call a function as a method
 
-console.dir(startGame);
+// console.dir(startGame);
+// a function, while having its own type is an Object
+// which would then make functions a Reference type and be stored in the heap
 
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', start);
