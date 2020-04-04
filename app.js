e6ada5afa9a71b94dccd23733bf4a247dda539c1
Tim Mac <ttiimmaacc@MacBook-Pro.local>
2020-04-04 15:21:35 +1100

    add function for program - with comments on functions inside objects

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is starting...');
+}
+// Standalone function
+
+// const person = {
+//   greet: function greet() {
+//     console.log('Hello there!');
+//   }
+// };
+// Basic function stored in an object is called a method.
+// Method is just a function attached to an object
+
+// person.greet();
+// call a function as a method
+
+startGameBtn.addEventListener('click', startGame);
