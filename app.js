0d90327e7c515d1bf6eb47655237b4b0b6e1eb87
Tim Mac <ttiimmaacc@MacBook-Pro.local>
2020-04-05 20:22:10 +1000

    add Rest operator, function inside of a function and callback function

--- app.js
+++ app.js
@@ -130,3 +130,36 @@ startGameBtn.addEventListener('click', () => {
 });
 // On the fly, Anonymous function - for use when you will never need to use the function inside of your code again.
 // function takes a function as an argument
+
+
+// not related to game
+
+
+
+// Using the rest operator
+//rest operator looks like the spread operator but instead of using {...} in an object or [...] in an array
+//Takes all arugments and merges into an array inside of a function
+
+const sumUp = (a, b, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
+  let sum = 0;
+  for (const num of numbers) {
+    sum += validateNumber(num);
+  }
+  return sum;
+};
+
+const subtractUp = function() {
+  let sum = 0;
+  for (const num of arguments) { // don't use that
+    sum -= num;
+  }
+  return sum;
+}
+
+console.log(sumUp(1, 5, 'fdsa', -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20));
